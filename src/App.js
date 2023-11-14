dc9965deb678d158ffd76bd98509aa6c488c3566
Jeyanth Kumar <54762572+Jeyanth2001@users.noreply.github.com>
2023-11-14 20:32:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ICANIO-DevOps
+          JEyanth-DevOps
         </a>
       </header>
     </div>
