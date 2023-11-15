222317cc8650423119b3a4a567bfa0bef8a333a6
Jeyanth Kumar <54762572+Jeyanth2001@users.noreply.github.com>
2023-11-15 15:35:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          JEYANTH_ICANIO
+          JEYANTH_ICANIO-COM
         </a>
       </header>
     </div>
